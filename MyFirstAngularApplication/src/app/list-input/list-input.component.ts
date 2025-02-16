80ac1515ae10e40dd7c68f20b9e8c44ed05b2e73
Vasilsa1109 <vasilisavygovskaya1109@gmail.com>
2025-02-16 19:39:20 +0000

    list-input && some style changes

---
+++ MyFirstAngularApplication/src/app/list-input/list-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-input',
+  imports: [],
+  templateUrl: './list-input.component.html',
+  styleUrl: './list-input.component.scss'
+})
+export class ListInputComponent {
+
+}
