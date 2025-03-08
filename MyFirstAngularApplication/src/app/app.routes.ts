901a1409e572d0e542e0e0fdfac812837c0955b3
Vasilsa1109 <vasilisavygovskaya1109@gmail.com>
2025-03-08 19:18:03 +0300

    some changes // 08.03

--- MyFirstAngularApplication/src/app/app.routes.ts
+++ MyFirstAngularApplication/src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import { Routes, provideRouter } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
-import { TodoComponent } from './components/todo/todo.component';
 import { TodoDetailComponent } from './components/pages/todo-detail/todo-detail.component';
 import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
+import { AboutComponent } from './components/pages/about/about.component';
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'todos', component: TodoComponent},
+    {path: 'todos', loadComponent:
+    () => import('../app/components/todo/todo.component')
+.then(m => m.TodoComponent)},
     {path: 'todos/:id', component: TodoDetailComponent},
-    {path: '**', component: NotFoundComponent},
-     {path: 'tasks', component: TaskListComponent},
+    {path: 'tasks', component: TaskListComponent},
+    {path: 'about', component: AboutComponent},
+    {path: '**', component: NotFoundComponent}
 ];
 
 export const appRouter = provideRouter(routes);
--- MyFirstAngularApplication/src/app/components/pages/todo-detail/todo-detail.component.ts
+++ MyFirstAngularApplication/src/app/components/pages/todo-detail/todo-detail.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-todo-detail',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './todo-detail.component.html',
   styleUrl: './todo-detail.component.scss'
 })
 export class TodoDetailComponent {
+
   todoId: number | null = null;
 
   constructor(private route: ActivatedRoute){
