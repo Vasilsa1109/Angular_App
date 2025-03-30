74ff5c83dac3772259bdff227bd7d8916e99e148
Vasilsa1109 <vasilisavygovskaya1109@gmail.com>
2025-03-30 19:59:03 +0100

    config changes // 30.03

--- MyFirstAngularApplication/src/vite.config.ts
+++ MyFirstAngularApplication/src/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import angular from "@analogjs/vite-plugin-angular";
-import tsconfigPaths from "vite-tsconfig-paths";
+// import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [angular(), tsconfigPaths()],
+  plugins: [angular()],
+  base: 'https://github.com/Vasilsa1109/Angular_App'
+  // tsconfigPaths()],
 });
